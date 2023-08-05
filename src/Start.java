28b9a5bedcebcd63af8414f58a075c4c9ec93da8
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-08-05 10:30:27 +0200

    rifatto

--- src/Start.java
+++ src/Start.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class Start {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         House house = new House();
-        System.out.println("Enter number of floors");
-        house.setFloorsNumber(scanner.nextInt());
 
+        System.out.println(" Enter the address of the house:");
+        String address = scanner.nextLine();
+        house.setAddress(address);
+
+        System.out.println("Enter floor number: ");
+        int floorNumber = scanner.nextInt();
+        house.setFloorsNumber(floorNumber);
         scanner.nextLine();
-        System.out.println("Enter adress");
-        house.setAddress(scanner.nextLine());
-
-        System.out.println("Enter resident names (comma separated):");
-        house.setResidentsNames(scanner.nextLine());
 
         String houseDetails = String.format("House details:\nFloors: %d\nAddres: %sResidents: %s",
-           house.getFloorsNumber(),
-           house.getAddress(),
-           house.getResidentsNames() );
-        
+                house.getFloorsNumber(),
+                house.getAddress(),
+                house.getResidentsNames());
+
         System.out.println(houseDetails);
     }
 }
